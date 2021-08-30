f5ea0c6b15bc9791ae3d288fb3c66ebbb976bfb9
mbonessi77 <antoniobonessi@yahoo.com>
2021-08-29 20:14:45 -0400

    Send simple email

--- node_server.js
+++ node_server.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "antoniobonessi@gmail.com",
+        pass: "shhhh. it's secret"
+    },
+    tls:{ rejectUnauthorized: false}
+})
+
+var mailOptions = {
+    from: 'antoniobonessi@yahoo.com',
+    to: 'antoniobonessi@gmail.com',
+    subject: "Test Email",
+    text: "Does this work?"
+}
+
+transporter.sendMail(mailOptions, function(err, info) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("Email Sent: " + info.response)
+    }
+})
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
