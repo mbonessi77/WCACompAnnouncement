2946db0a17dc72de99d06ef862338776dd5b6660
mbonessi77 <antoniobonessi@yahoo.com>
2021-08-30 10:50:27 -0400

    Filter comps by country

--- node_server.js
+++ node_server.js
@@ -2,24 +2,55 @@ var express = require('express');
 var app = express();
 var nodemailer = require('nodemailer')
 var schedule = require('node-schedule')
+var https = require('https')
 
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "antoniobonessi@gmail.com",
-        pass: "it's a secret. shhhhhhhhh"
-    },
-    tls:{ rejectUnauthorized: false}
+var compList
+
+https.get("https://www.worldcubeassociation.org/api/v0/competitions", (resp) => {
+    let data = ''
+
+    resp.on('data', (chunk => {
+        data += chunk
+    }))
+
+    resp.on('end', () => {
+        compList = JSON.parse(data)
+    })
+}).on('error', (err) => {
+    console.log("Error: " + err.message)
 })
 
-var mailOptions = {
-    from: 'antoniobonessi@yahoo.com',
-    to: 'antoniobonessi@gmail.com',
-    subject: "Test Email",
-    text: "Does this work?"
-}
+schedule.scheduleJob('* * * * *', () => {
+    notifyNewComps()
+})
 
-schedule.scheduleJob('0 * * * *', () => {
+function notifyNewComps() {
+    var filteredComps = compList.filter(function(event) {
+        return event.country_iso2 == "US"
+    })
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "antoniobonessi@gmail.com",
+            pass: "asdf"
+        },
+        tls:{ rejectUnauthorized: false}
+    })
+
+    var emailText = "A new WCA competition was just announced for your country. Details for the competition(s) are below\n\n"
+
+    for (var i = 0; i < filteredComps.length; i++) {
+        emailText += filteredComps[i].name + ": " + filteredComps[i].url + "\n"
+    }
+    
+    var mailOptions = {
+        from: 'antoniobonessi@yahoo.com',
+        to: 'antoniobonessi@gmail.com',
+        subject: "New Competition In Your Country!",
+        text: emailText
+    }
+
     transporter.sendMail(mailOptions, function(err, info) {
         if (err) {
             console.log(err)
@@ -27,7 +58,7 @@ schedule.scheduleJob('0 * * * *', () => {
             console.log("Email Sent: " + info.response)
         }
     })
-})
+}
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
@@ -37,5 +68,5 @@ var server = app.listen(8080, function () {
     var host = server.address().address
     var port = server.address().port
     
-    console.log("Example app listening on port %s", host, port)
+    console.log("Listening on port %s", host, port)
  })
