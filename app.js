81877021dfa482cba170351c7907f04c65160764
mbonessi77 <antoniobonessi@yahoo.com>
2021-08-31 12:05:02 -0400

    Sort comps by country

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var schedule = require('node-schedule')
 var https = require('https')
 
 var compList
+var compByCountry = new Map()
+
 var userList = [{
                  user_email: "antoniobonessi@gmail.com",
                  user_country: "US"
@@ -14,23 +16,102 @@ var userList = [{
                  user_country: "NZ"
                  }]
 
-https.get("https://www.worldcubeassociation.org/api/v0/competitions", (resp) => {
-    let data = ''
-
-    resp.on('data', (chunk => {
-        data += chunk
-    }))
-
-    resp.on('end', () => {
-        compList = JSON.parse(data)
-    })
-}).on('error', (err) => {
-    console.log("Error: " + err.message)
-})
-
 schedule.scheduleJob('* * * * *', () => {
-    notifyNewComps()
+    fetchCompList()
 })
+
+function storeCurrentCompList(comp_list) {
+    if(compList != null) {
+        compList = compList.filter(function(event) {
+            var startDate = event.start_date
+            var dateArr = startDate.split("-")
+            var year = dateArr[0]
+            var month = dateArr[1]
+            var day = dateArr[2]
+
+            var currentDate = new Date()
+            var currentYear = currentDate.getFullYear()
+            var currentMonth = currentDate.getMonth() + 1 //Default is 0-11
+            var currentDay = currentDate.getDate()
+
+            var isFutureComp = true
+
+            if (year < currentYear) {
+                isFutureComp = false
+            } else if (month < currentMonth && year <= currentYear) {
+                isFutureComp = false
+            } else if (day < currentDay && month <= currentMonth && year <= currentYear) {
+                isFutureComp = false
+            }
+
+            return isFutureComp
+        })
+    }
+
+    if (JSON.stringify(compList) === JSON.stringify(comp_list)) {
+        return
+    }
+
+    compList = comp_list
+
+    sortListIntoMap(compList)
+
+    notifyNewComps()
+}
+
+function sortListIntoMap(list) {
+    list.forEach(element => {
+        if (compByCountry.has(element.country_iso2)) {
+            compByCountry.get(element.country_iso2).push(element)
+        } else {
+            compByCountry.set(element.country_iso2, [])
+            compByCountry.get(element.country_iso2).push(element)
+        }
+    });
+
+    compByCountry.forEach(list => {
+        list = list.filter(function(event) {
+            var startDate = event.start_date
+            var dateArr = startDate.split("-")
+            var year = dateArr[0]
+            var month = dateArr[1]
+            var day = dateArr[2]
+
+            var currentDate = new Date()
+            var currentYear = currentDate.getFullYear()
+            var currentMonth = currentDate.getMonth() + 1 //Default is 0-11
+            var currentDay = currentDate.getDate()
+
+            var isFutureComp = true
+
+            if (year < currentYear) {
+                isFutureComp = false
+            } else if (month < currentMonth && year <= currentYear) {
+                isFutureComp = false
+            } else if (day < currentDay && month <= currentMonth && year <= currentYear) {
+                isFutureComp = false
+            }
+
+            return isFutureComp
+        })
+    })
+}
+
+function fetchCompList() {
+    https.get("https://www.worldcubeassociation.org/api/v0/competitions", (resp) => {
+        let data = ''
+
+        resp.on('data', (chunk => {
+            data += chunk
+        }))
+
+        resp.on('end', () => {
+            storeCurrentCompList(JSON.parse(data))
+        })
+    }).on('error', (err) => {
+        console.log("Error: " + err.message)
+    })
+}
 
 function notifyNewComps() {
 
@@ -38,24 +119,22 @@ function notifyNewComps() {
         service: 'gmail',
         auth: {
             user: "antoniobonessi@gmail.com",
-            pass: "rwyfrvtcjbpmzmoz"
+            pass: ""
         },
         tls:{ rejectUnauthorized: false}
     })
 
     for (var i = 0; i < userList.length; i++) {
-        var filteredComps = compList.filter(function(event) {
-            return event.country_iso2 == userList[i].user_country
-        })
+        var compsToNotify = compByCountry.get(userList[i].user_country)
 
-        filteredComps = filteredComps.filter(function(comp) {
+        compsToNotify = compsToNotify.filter(function(comp) {
             return comp.cancelled_at == null
         })
 
         var emailText = "A new WCA competition was just announced for your country. Details for the competition(s) are below.\n\n"
 
-        for (var k = 0; k < filteredComps.length; k++) {
-            emailText += filteredComps[k].name + ": " + filteredComps[k].url + "\n\n"
+        for (var k = 0; k < compsToNotify.length; k++) {
+            emailText += compsToNotify[k].name + ": " + compsToNotify[k].url + "\n\n"
         }
         
         var mailOptions = {
